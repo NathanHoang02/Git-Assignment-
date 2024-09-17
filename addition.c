c5a8962d92b4b8a66414dfc12cf8e2d49354547c
Nathan Hoang <nhoang6@lsu.edu>
2024-09-16 20:02:38 -0500

    Committed addition.c

---
+++ addition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+        int a, b, sum = 0; 
+	printf("Enter two integers: "); 
+	scanf("%d %d", &a, &b); 
+	
+	sum = a + b; 
+	printf("Sum is %d", sum); 
+	
+	return 0;
+}
+
