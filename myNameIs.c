fda412ac35391e172cf0c84ebc69bdbe62ac41bb
Nathan Hoang <nhoang6@lsu.edu>
2024-09-16 20:01:24 -0500

    Committed myNameIs

---
+++ myNameIs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+        char name[100];
+
+        printf ("Please Enter Your Name: ")l
+        scanf("%s", name);
+        printf("My Name is %s\n", name);
+        return 0;
+}
+
+
